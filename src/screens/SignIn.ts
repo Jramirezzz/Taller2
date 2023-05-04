733b0376494802ecbabf05fa7fa5aa0dffda1f74
Juan Esteban Ramirez Perdomo <115906656+Jramirezzz@users.noreply.github.com>
2023-05-04 16:41:48 -0500

    Update SignIn.ts

---
+++ src/screens/SignIn.ts
@@ -0,0 +1,45 @@
+const information = { email: "", password: "" };
+
+class Singin extends HTMLElement {
+
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"})
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+    render() {
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = ``;
+
+            const css = this.ownerDocument.createElement('style')
+                css.innerHTML = styles
+                this.shadowRoot?.appendChild(css);
+
+            const form = this.ownerDocument.createElement('section')
+
+            const Email = this.ownerDocument.createElement('input')
+            Email.placeholder = "Email"
+            Email.type = "Email"
+            Email.addEventListener("change", (cha:any) => (information.email = cha.target.value));
+            this.shadowRoot?.appendChild(Email)
+
+            const Password = this.ownerDocument.createElement('input')
+            Password.placeholder = "Password"
+            Password.type = "Password"
+            Password.addEventListener("change", (cha:any) => (information.password = cha.target.value));
+            this.shadowRoot?.appendChild(Password)
+                
+            const button = this.ownerDocument.createElement('my-boton');
+            form.appendChild(button)
+            this.shadowRoot?.appendChild(form)
+
+
+        }
+    }
+}
+
+customElements.define('My-singin', Singin)
